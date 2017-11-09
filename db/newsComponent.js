868208835a2c32fcac6e63d6c5b5a22a526e8d2d
qiudaoermu <1540274138@qq.com>
2017-11-09 23:09:04 +0800

    no message

---
+++ db/newsComponent.js
@@ -0,0 +1,13 @@
+/* 1 */
+{
+    "_id" : ObjectId("5978979fce771dca2903c05d"),
+    "word" : "人大教师妻子失联3天后死亡 警方对此事正进行调查",
+    "img" : "Comnponent0_img"
+}
+
+/* 2 */
+{
+    "_id" : ObjectId("597897a9ce771dca2903c068"),
+    "word" : "达赖将以贵宾身份访问博茨瓦纳 外交部：不会容忍",
+    "img" : "component1_img"
+}
